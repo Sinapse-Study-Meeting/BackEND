80567e92bc0d5239cd4520f0b9a6dbad10a89cba
JoaoRuani <jvitornegri@gmail.com>
2020-05-14 18:40:12 -0300

    Modificações na view

---
+++ resources/views/layouts/search-bar.blade.php
@@ -0,0 +1,12 @@
+<section class="search-bar">
+    <div class="container">
+        <a role="button" class="search-bar__trigger">
+            <i class='bx bx-search search-bar-icon'></i>
+        </a>
+        <form action="#" class="search-bar__form">
+            <label for="search" class="search-bar__label">Busque por usuários ou grupos</label>
+            <input type="search" placeholder="Digite aqui sua busca">
+            <button type="submit">buscar</button>
+        </form>
+    </div>
+</section>
