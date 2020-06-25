707da35cbe34d17d28e3d5747ffdff19c2be0575
Janaina2000 <janaina_2012silva@hotmail.com>
2020-06-25 13:09:44 -0300

    trabalho marcelo

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -5,52 +5,57 @@
 <meta name="keywords" content="Sinapse, estudar, grupos, login">
 @endsection
 @section('css')
-<link rel="stylesheet" href="{{asset('css/home.min.css')}}">
-
+<link rel="stylesheet" href="{{asset('css/admin.min.css')}}">
 
 @endsection
 @section('content')
 @include("layouts.navbar")
 
-<table>
-    <tr>
-        <th>Nome</th>
-        <th>Email</th>
-        <th>Estado</th>
-        <th>Cidade</th>
-    </tr>
-    @foreach ($relatorio as $item)
-        <tr>
-            <td>{{$item->name}}</td>
-            <td>{{$item->email}}</td>
-            <td>{{$item->state}}</td>
-            <td>{{$item->city}}</td>
-        </tr>
-    @endforeach
-</table>
-<br>
-<form action="{{route('admin.atualizar_relatorio_user')}}" method="POST">
-    @csrf
-    <button type="submit">ATUALIZAR RELATÓRIO</button>
-</form>
+<div class="admin-principal">
+    <div class="admin">
+        <div class="inicio">
+            <table>
+                <tr class="colInicio">
+                    <th>Usuarios</th>
+                    <th>Interesses</th>
+                    <th>Mensagens</th>
+                </tr>
+                @foreach (DB::table('relatorio_geral')->get() as $item)
+                <tr class="colInicio">
+                    <td>{{$item->qtd_usuarios}}</td>
+                    <td>{{$item->qtd_interesses}}</td>
+                    <td>{{$item->qtd_mensagens}}</td>
+                </tr>
+                @endforeach
 
-<hr>
-<table>
-    <tr>
-        <th>Quantidade de usuarios</th>
-        <th>Quantidade de interesses</th>
-        <th>Quantidade de mensagens</th>
-    </tr>
-    @foreach (DB::table('relatorio_geral')->get() as $item)
-        <tr>
-            <td>{{$item->qtd_usuarios}}</td>
-            <td>{{$item->qtd_interesses}}</td>
-            <td>{{$item->qtd_mensagens}}</td>
-        </tr>
-    @endforeach
+            </table>
+            <form action="{{route('admin.atualizar_relatorio_user')}}" method="POST">
+                @csrf
+                <button type="submit">Atualizar Relatório</button>
+            </form>
+        </div>
+        <div class="admin-conteudo">
+            <h2 class="h1-size">Usuários do Sistema</h2>
+            <table class="listagem">
+                <tr class="col-principal">
+                    <th>Nome</th>
+                    <th>E-mail</th>
+                    <th>Estado</th>
+                    <th>Cidade</th>
+                </tr>
+                @foreach ($relatorio as $item)
+                <tr>
+                    <td>{{$item->name}}</td>
+                    <td>{{$item->email}}</td>
+                    <td>{{$item->state}}</td>
+                    <td>{{$item->city}}</td>
+                </tr>
+                @endforeach
+            </table>
 
-</table>
-
+        </div>
+    </div>
+</div>
 
 
 @section('scripts')
