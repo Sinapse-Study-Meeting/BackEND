fd33410f9bc8ae1f2620ccd512586549ea9d99da
JoaoRuani <jvitornegri@gmail.com>
2020-07-10 22:26:34 -0300

    Arrumando alinhamento do texto no index

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,38 +1,32 @@
 @extends('layouts.base')
-@include('vendor.bootstrap')
 @section('title',"Bem-vindo Sinapse")
 @section('metas')
-<meta name="description" content="A plataforma para você encontrar sua companhia de estudos">
-<meta name="keywords" content="Sinapse, estudar, grupos, login">
-@endsection
-@section('bootstrap_css')
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-<link rel="stylesheet" href="../vendor/bootstrap/css/bootstrap.min.css">
+    <meta name="description" content="A plataforma para você encontrar sua companhia de estudos">
+    <meta name="keywords" content="Sinapse, estudar, grupos, login">
 @endsection
 @section('css')
-<link rel="stylesheet" href="{{ asset('css/welcome.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/welcome.min.css') }}">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 @endsection
-@section('content')
-<!-- Fonts -->
-<link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
+@section('content')
 <header class="navbar__home">
-    <a href="{{route('home')}}" class="logo">
+    <a href="{{ route('home') }}" class="logo">
         <h1 class="logo__replacement__white">Sinapse</h1>
     </a>
     <nav class="navbar__menu__home">
-        @if (Route::has('login'))
-        <div class="top-right links">
-            @auth
-            <a href="{{ url('/home') }}">Home</a>
-            @else
-            <a href="{{ route('login') }}">Login</a>
+        @if(Route::has('login'))
+            <div class="top-right links">
+                @auth
+                    <a href="{{ url('/home') }}">Home</a>
+                @else
+                    <a href="{{ route('login') }}">Login</a>
 
-            @if (Route::has('register'))
-            <a href="{{ route('register') }}">Cadastrar</a>
-            @endif
-            @endauth
-        </div>
+                    @if(Route::has('register'))
+                        <a href="{{ route('register') }}">Cadastrar</a>
+                    @endif
+                @endauth
+            </div>
         @endif
     </nav>
 </header>
@@ -51,7 +45,8 @@
 <!-- <footer class="footer-inicial">
 
 </footer> -->
+@endsection
 @section('scripts')
-<script src="{{asset('js/toggle.js')}}"></script>
-<script src="{{asset('js/mostrarsenha.js') }}"></script>
+<script src="{{ asset('js/toggle.js') }}"></script>
+<script src="{{ asset('js/mostrarsenha.js') }}"></script>
 @endsection
